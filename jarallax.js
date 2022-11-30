14e42ca3e79678dae8369ecdc2459d36e7412ce6
kallamkeerthisree <115975095+kallamkeerthisree@users.noreply.github.com>
2022-11-30 15:24:02 +0530

    Update jarallax.js

--- jarallax.js
+++ jarallax.js
@@ -1,9 +1,3 @@
-/*!
- * Name    : Just Another Parallax [Jarallax]
- * Version : 1.1.0
- * Author  : _nK http://nkdev.info
- * GitHub  : https://github.com/nk-o/jarallax
- */
 (function(factory) {
     'use strict';
     if (typeof define === 'function' && define.amd) {
